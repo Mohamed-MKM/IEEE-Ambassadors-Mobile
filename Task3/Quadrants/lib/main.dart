62b02365552d81631291b9e29b1568f7568ef6af
MMk <Mohamedmk2004@gmail.com>
2024-08-28 02:50:40 +0300

    Task3 updated

--- Task3/Quadrants/lib/main.dart
+++ Task3/Quadrants/lib/main.dart
@@ -11,12 +11,3 @@ int quadrant(int x, int y) {
   
   return 0; 
 }
-
-void main() {
- 
-  print(quadrant(1, 2));     
-  print(quadrant(3, 5));     
-  print(quadrant(-10, 100)); 
-  print(quadrant(-1, -9));   
-  print(quadrant(19, -56));  
-}
